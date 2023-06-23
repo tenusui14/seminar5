c0a0e99f28c3bf3c75ba216c2098f76394bea9ef
Artem Tsytsylin <workhardgofock@gmail.com>
2023-06-23 21:00:13 +0300

    task32

---
+++ task32/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+int[] GetRandomArray(){
+    int [] array = new int[20];
+    for(int i = 0; i < array.Length; i++){
+        array[i] = Random.Shared.Next(-10, 11);
+    }
+    return array;
+}
+int[] SwapDigit(int [] array){
+    for(int i = 0; i < array.Length; i++){
+        array[i] = array[i] * -1;
+    }
+    return array;
+}
+
+int[] myArray = GetRandomArray();
+Console.WriteLine($"[ {string.Join(", ",myArray)} ]");
+int[] swap = SwapDigit(myArray);
+Console.WriteLine($"[ {string.Join(", ",swap)} ]");
