502bcc9fa4951b5bfa3a1b094f70cc26e29f6c58
Artem Tsytsylin <workhardgofock@gmail.com>
2023-06-24 23:00:48 +0300

    HW task34 is done

---
+++ homework_task34/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве. [345, 897, 568, 234] -> 2
+
+int[] GetRandomArray(){
+    int [] array = new int[5];
+    for(int i = 0; i < array.Length; i++){
+        array[i] = Random.Shared.Next(100, 1000);
+    }
+    return array;
+}
+int Calculations(int[] array){
+int count = 0;
+for(int i = 0; i < array.Length; i++){
+    if (array[i] % 2 == 0){
+        count++;
+    }
+}
+return count;
+}
+
+int[] myArray = GetRandomArray();
+int getEven = Calculations(myArray);
+Console.WriteLine($"[ {string.Join(", ",myArray)} ] -> {getEven}");
